4e21c1656b3fc9b43d09ede8c62a6268b25a1fb0
akshar36 <112136517+akshar36@users.noreply.github.com>
2023-09-19 13:12:55 -0700

    ChaseTag_V1

---
+++ Assets/Evader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Evader : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 900.0f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector2 moveDirection = new Vector2(moveInput, 0);
+
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit the ground");
+        isGrounded = true;
+    }
+}
